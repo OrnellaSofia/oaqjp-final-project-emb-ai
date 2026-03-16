2b3212c43df1fb5a0783cc08bc898c3b1107ed86
OrnellaSofia <orneacerbi@gmail.com>
2026-03-16 02:53:41 -0400

    emotion detection function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,20 @@
-ANALYSIS_URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+import requests
 
 def emotion_detection(text_to_analyze):
-    pass
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    
+    payload = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+    
+    response = requests.post(url, headers=headers, json=payload)
+    
+    return response.json()
+
+print(emotion_detection("I am so upset"))
